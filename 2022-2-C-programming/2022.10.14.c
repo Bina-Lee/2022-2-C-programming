d1c1beb23bf5df21c9560fa3ce68f3d38f83c608
Herb <Herb@DESKTOP-C583OK2>
2022-10-14 16:06:37 +0900

    수업내욤ㄴㅇㄹ

---
+++ 2022-2-C-programming/2022.10.14.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int* p
+//-> 포인터가 가리키는건 포인터
+
+//데이터 작성
+//p=(int *) 1776; //지양할 것
+//절대번지 지정...
+//메뉴얼에 주소가 정해진 경우 사용
+//윈도우 리눅스는 잘 안씀
+
+//&는 주소를 반환하는 '연산자'
+//기호 아님
+
+//Type Cast
+//ex) (float) i;
+
+//define
+//앞의 것을 뒤로 대체
+
+//avg_score = total_score/sn;
+
+//a[0] == *(iptr+0)
