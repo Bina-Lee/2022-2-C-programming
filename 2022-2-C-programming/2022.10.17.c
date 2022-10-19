a19d57beb11d8c741f7b41cb65fd2af6a4a080a4
Herb <Herb@DESKTOP-C583OK2>
2022-10-19 12:39:43 +0900

    asdf

---
+++ 2022-2-C-programming/2022.10.17.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//숙제
+//1. Integer 2차원 배열 5*5, 16진수 형태로 초기값 부여
+//2. char 포인터 변수 하나로 25개를 모두 찾아 출력, 각 바이트의 값도 순서대로 4개씩 구분하여 출력
+//3. 배열의 값과 각 값의 바이트값이 순서대로 출력됐는지 확인
+//4. warning이 하나도 없도록 제작
+
+//
+
+//call by value 값만 전달
+//call by reference 주소까지 전달
+
+//p[5] == *(p+5)
