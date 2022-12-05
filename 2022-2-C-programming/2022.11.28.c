f1092475a5ed3d804c301cb5b4af283abf548d3e
Herb <Herb@DESKTOP-C583OK2>
2022-12-05 14:50:25 +0900

    asdf

--- 2022-2-C-programming/2022.11.28.c
+++ 2022-2-C-programming/2022.11.28.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-#if 1
+#if 0
 int main() {
 
 }
@@ -34,7 +34,7 @@ int main() {
 //사용할때마다 캐스팅을 해줘야됨
 //*(*int)voidpointer = value;
 
-#if 1
+#if 0
 int main(int argc, char* argv[]) {
 	for (int i = 0; i < argc; i++)printf("%d %s", i, argv[i]);
 }
@@ -42,5 +42,48 @@ int main(int argc, char* argv[]) {
 
 
 //이진탐색
+#if 1
+int main() {
+	int data[100];
+	for (int i = 0; i < 100; i++)data[i] = i + 100;
+	int minindex = 0;
+	int maxindex = 99;
+	int index = (maxindex + minindex) / 2;
+	int finddata = 186;
+	while (data[index] != finddata) {
+		if (data[index] < finddata)minindex = index;
+		else if (data[index] > finddata)maxindex = index;
+		index = (minindex + maxindex) / 2;
+	}
+	printf("%d", index);
+}
+#endif
 //생년월일을 받아서 만 몇살인지 출력
+#if 0
+int main() {
+	int birth;
+	scanf("%d", &birth);
+	int year, month, day;
+	year = birth / 10000;
+	month = birth / 100;
+	month = month % 100;
+	day = birth % 100;
+	int age = 0;
+	if (month < 11) { age = 2022 - year; printf("%d", age); return 0;; }
+	else if (month == 12) { age = 2021 - year; printf("%d", age); return 0; }
+	else {
+		if (day <= 28) { age = 2022 - year; printf("%d", age); return 0; }
+		else { age = 2021 - year; printf("%d", age); return 0; }
+	}
+}
+#endif
 //char* 변수 한개(배열첫자) 이외에 변수 하나만 정의하여 strlen구현
+#if 0
+int main() {
+	char str[100];
+	scanf("%s", str);
+	int i = 0;
+	while (str[i] != 0)i++;
+	printf("%d", i);
+}
+#endif
